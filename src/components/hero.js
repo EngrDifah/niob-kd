424de0a8afaec938aeb84e62886364d6bc948520
EngrDifah <43475641+EngrDifah@users.noreply.github.com>
2023-07-05 06:51:52 +0100

    first commit

---
+++ src/components/hero.js
@@ -0,0 +1,54 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+var heroData = [
+  {
+    id: 1,
+    image: require('../assets/images/img-hero1.jpg'),
+    title: 'Do you Know That..',
+    description: 'Builders do not just build? They also engage in: Planning, Designing, Costing, Production and, Building Maintenance.',
+    link: 'https://www.google.com'
+  },
+  {
+    id: 2,
+    image: require('../assets/images/img-hero2.jpg'),
+    title: 'Welcome To NIOB Kaduna Chapter',
+    description: 'The Nigerian Institute of Building is the professional body for Builders and those who are about to be engaged in the Building Profession.',
+    link: 'https://www.facebook.com'
+  },
+  {
+    id: 3,
+    image: require('../assets/images/img-hero3.jpg'),
+    title: 'Enjoy the Difference',
+    description: 'Do you have the relevant academic qualifications and would want to become a Registered Professional Builder?',
+    link: 'https://www.twitter.com'
+  }
+]
+
+function AppHero() {
+  return (
+    <section id="home" className="hero-block">
+       <Carousel>
+          {
+            heroData.map(hero => {
+              return (
+                <Carousel.Item key={hero.id}>
+                  <img
+                    className="d-block w-100"
+                    src={hero.image}
+                    alt={"slide " + hero.id}
+                  />
+                  <Carousel.Caption>
+                    <h2>{hero.title}</h2>
+                    <p>{hero.description}</p>
+                    <a className="btn btn-primary" href={hero.link}>Register Now <i className="fas fa-chevron-right"></i></a>
+                  </Carousel.Caption>             
+                </Carousel.Item>
+              );
+            })
+          }
+      </Carousel>
+    </section>
+  );
+}
+
+export default AppHero;
